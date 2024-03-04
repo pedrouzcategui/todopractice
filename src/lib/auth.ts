d0fbf7c6073959654fb97f2f6f55067b90113a4a
Juan Alvarez <juanandres140299@gmail.com>
2024-03-04 18:26:36 -0400

    set barebones structure

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+
+import { db } from "@/lib/db";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const authConfig: NextAuthOptions = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+};
