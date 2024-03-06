4f9a5e99dae91a9025f08e52ae10c3f5802bff28
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-03-05 23:26:24 -0400

    feature(dashboard): implement base dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-export default function DashboardPage() {
+const VIEW_MODES = {
+    KANBAN: "kanban",
+    LIST: "list"
+} as const
+
+type ViewMode = keyof typeof VIEW_MODES
+
+/**
+ * Expected query parameters for `DashboardPage`
+ */
+type SearchParams = {
+    view: ViewMode
+}
+
+export default function DashboardPage({ searchParams }: { searchParams: SearchParams } ) {
+    const { view: defaultViewMode } = searchParams
+
     // TODO: workspaces should be populated with users workspaces provided by backend
     const WORKSPACES = [
         {
@@ -20,8 +37,8 @@ export default function DashboardPage() {
     ]
 
     return (
-        <main className="min-h-screen p-12">
-            <nav className="flex flex-col gap-4">
+        <main className="min-h-screen p-12 flex flex-col gap-8">
+            <nav className="flex-grow-0">
                 <div className="flex gap-6">
                     <Select>
                         <SelectTrigger className="w-1/5">
@@ -45,13 +62,30 @@ export default function DashboardPage() {
                         <AvatarFallback>GO</AvatarFallback>
                     </Avatar>
                 </div>
-
-                <div>
-
-                </div>
             </nav>
 
-            <section className="h-full">
+            <section className="flex flex-col flex-grow h-full">
+                {/* TODO: `defaultValue` should come from query parameters in order to provide the state of the current view */}
+                <Tabs defaultValue={defaultViewMode} className="flex flex-col flex-grow">
+                    <header className="flex flex-col gap-1 items-center flex-grow-0">
+                        <TabsList className="px-3 py-6">
+                            <TabsTrigger className="w-[100px]" value={VIEW_MODES.KANBAN}>Kanban</TabsTrigger>
+                            <TabsTrigger className="w-[100px]" value={VIEW_MODES.LIST}>List</TabsTrigger>
+                        </TabsList>
+                    </header>
+
+                    <div className="flex justify-center items-center flex-grow">
+                        <TabsContent value={VIEW_MODES.KANBAN}>
+                            {/* TODO: should render Kanban visualization component */}
+                            <p>This is the kanban</p>
+                        </TabsContent>
+
+                        <TabsContent value={VIEW_MODES.LIST}>
+                            {/* TODO: should render List visualization component */}
+                            <p>This is the list</p>
+                        </TabsContent>
+                    </div>
+                </Tabs>
             </section>
         </main>
     )
