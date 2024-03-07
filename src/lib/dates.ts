6f294aa04e1ca147a783e6da03538f6d984a6989
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-03-07 00:32:28 -0400

    feature(dashboard): implement core tasks list components and list view mode

---
+++ src/lib/dates.ts
@@ -0,0 +1,13 @@
+/**
+ * Default config for long date format in en-US locale.
+ */
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    dateStyle: "long",
+})
+
+/**
+ * Formats given date based on the `dateFormatter` config. 
+ */
+export function formatDate(date: Date) {
+  return dateFormatter.format(date)
+}
