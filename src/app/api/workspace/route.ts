672bd1698fa2bb82d4f6dc9c7df39452f0c5c3ed
Juan Alvarez <juanandres140299@gmail.com>
2024-03-08 22:35:41 -0400

    add first route logic. add withAuth closure. add user types to next-auth

--- src/app/api/workspace/route.ts
+++ src/app/api/workspace/route.ts
@@ -1,7 +1,29 @@
-import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-export function POST(req: NextRequest) {
-  return NextResponse.json({
-    message: "Hello from the workspace API route!",
+export async function POST() {
+  return withAuth(async (req, session) => {
+    const data: Prisma.WorkspaceCreateInput = await req.json();
+    const { user } = session;
+
+    if (!user?.id) {
+      return NextResponse.json({ message: "Bad Input" }, { status: 402 });
+    }
+
+    const workspace = await db.workspace.create({
+      data: {
+        ...data,
+        users: {
+          create: {
+            userId: user?.id,
+            isAdmin: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(workspace);
   });
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth, { type NextAuthOptions } from "next-auth";
+import NextAuth, { Session, type NextAuthOptions } from "next-auth";
 
 import { db } from "@/lib/db";
 import { remember } from "@epic-web/remember";
@@ -7,6 +7,8 @@ import { remember } from "@epic-web/remember";
 import Discord from "next-auth/providers/discord";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./user";
 
 export const authConfig: NextAuthOptions = {
   providers: [
@@ -26,6 +28,28 @@ export const authConfig: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   pages: { signIn: "/login" },
   session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, user }) {
+      return {
+        ...session,
+        user,
+      };
+    },
+  },
 };
 
 export const auth = remember("auth", () => NextAuth(authConfig));
+
+export function withAuth(
+  handler: (req: NextRequest, session: Session) => Promise<NextResponse>
+) {
+  return async (req: NextRequest) => {
+    const session = await getSession();
+
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    return handler(req, session);
+  };
+}
