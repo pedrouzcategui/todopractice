1a3b9d3200aa098035a10bbb88140ae7157f9d3c
Juan Alvarez <juanandres140299@gmail.com>
2024-03-10 17:14:01 -0400

    updates: preliminar workspaces cards

--- src/app/(private)/dashboard/workspaces/page.tsx
+++ src/app/(private)/dashboard/workspaces/page.tsx
@@ -10,28 +10,36 @@ export default async function WorkspacesPage({ searchParams }: PageProps) {
   const { user } = (await getSession()) ?? {};
   if (!user) return null;
 
-  const userWorkspaces = (
-    await db.user.findFirst({
-      where: {
-        AND: [
-          { id: user.id },
-          {
-            workspaces: {
-              some: { workspace: { title: { contains: searchParams.search } } },
-            },
+  const userWorkspaces = await db.workspace.findMany({
+    where: {
+      AND: [
+        {
+          users: {
+            some: { userId: user.id },
           },
-        ],
-      },
-      include: {
-        workspaces: {
-          include: { workspace: true },
         },
-      },
-    })
-  )?.workspaces;
+        {
+          OR: [
+            {
+              title: {
+                contains: searchParams.search ?? "",
+                mode: "insensitive",
+              },
+            },
+            {
+              description: {
+                contains: searchParams.search ?? "",
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
 
   return (
-    <main className="p-16">
+    <main className="p-4 bg-background">
       <header className="mb-12">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Workspaces
@@ -41,9 +49,29 @@ export default async function WorkspacesPage({ searchParams }: PageProps) {
         </p>
       </header>
 
-      {JSON.stringify(userWorkspaces, null, 2)}
-
       <Controls />
+
+      <section className="grid md:grid-cols-2 lg:grid-cols-3 mt-12 gap-8">
+        {userWorkspaces?.map(({ id, title, description }) => (
+          <article
+            key={id}
+            className="flex gap-6 p-6 shadow bg-card text-card-foreground border border-border/50 rounded-lg"
+          >
+            <figure className="min-w-[100px] min-h-[100px]">
+              <img
+                src={`https://source.unsplash.com/100x100/?${title}`}
+                alt=""
+                className="rounded-lg"
+              />
+            </figure>
+
+            <div>
+              <h2 className="text-2xl font-bold mb-1">{title}</h2>
+              <p className="text-muted-foreground">{description}</p>
+            </div>
+          </article>
+        ))}
+      </section>
     </main>
   );
 }
