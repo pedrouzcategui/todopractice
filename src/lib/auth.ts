3f5001e4842f4f80de03084083eacf76b5ca146a
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-03-13 22:30:20 -0400

    feature(auth): consume name in jwt and session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,6 +38,7 @@ export const authConfig: NextAuthOptions = {
 
       token.id = user.id;
       token.redirectToTutorial = !user.isTutorialFinished;
+      token.name = user.name;
 
       return token;
     },
@@ -47,6 +48,7 @@ export const authConfig: NextAuthOptions = {
         user: {
           ...session.user,
           id: token.id,
+          name: token.name,
           redirectToTutorial: token.redirectToTutorial,
         },
       };
